a396d8fd0be3d214180d59ff66a9328bcf0efab8
Raul <r.oyarzun83@gmail.com>
2023-09-03 13:29:03 -0300

    CRUD MongoDB operativo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from '../models/User.model.js'
+//import bcrypt from 'bcrypt'
 
 
 // Ver lista de usuarios
@@ -26,17 +27,32 @@ export const createUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
     try {
-        const userRut = req.param.rut
+        const userRut = req.params.rut
         const updateData = req.body
 
         const updateUser = await User.findOneAndUpdate({ rut: userRut }, updateData, { new: true })
 
         if (!updateUser) {
-            return res.status(404).json({ message: 'Usuario no encontrado. ' })
+            return res.status(404).json({ message: 'Usuario no encontrado.' })
         }
-        res.status(202).json({ message: `Usuario ${updateUser.nombre} ${updateUser.apellido} actualizado. ` })
+        res.status(202).json({ message: `Usuario ${updateUser.nombre} ${updateUser.apellido} actualizado.` })
 
     } catch (error) {
-
+        res.status(500).json({ message: 'No se actualizo el usuario' })
     }
 }
+
+export const deleteUserByRut = async (req, res) => {
+    try {
+        const userRut = req.params.rut
+        const removeUser = await User.findOneAndDelete({ rut: userRut })
+        if (!removeUser) {
+            return res.status(404).json({ message: 'Usuario no encontrado para eliminar' })
+        }
+
+        res.status(202).json({ message: `Usuario ${removeUser.nombre} ${removeUser.apellido} fue eliminado.` })
+    } catch (error) {
+        res.status(500).json({ message: 'Usuario no eliminado' })
+    }
+}
+
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
 
-import { createUser, getAllUsers, updateUser } from '../controllers/user.controller.js'
+import { createUser, getAllUsers, updateUser, deleteUserByRut } from '../controllers/user.controller.js'
 
 router.get('/users', getAllUsers)
 router.post('/users', createUser)
-router.put('./users/:rut', updateUser)
-
+router.put('/users/:rut', updateUser)
+router.delete('/users/:rut', deleteUserByRut)
 
 export default router
